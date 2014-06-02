49277ce848756afaf8cdaa8854eca170f0a86297
Sérgio Alves <sergioalves87@gmail.com>
2014-06-02 12:45:37 +0100

    Add cookie to tool StrongNet Upload

--- source/000/tools/strongnetupload/config-dist.php
+++ source/000/tools/strongnetupload/config-dist.php
@@ -1,7 +1,12 @@
 <?php
-$ftpDirsStrongNetTools = 'M:\Dev\Repo\VB.net\strongnet.tools\StrongNet.Tools\bin\Release\\';
-$ftpDirsStrongNetG4Setup = 'M:\Dev\Repo\VB.net\g4-setup\G4Setup\bin\Release\\';
-$ftpDirsStrongNetG4Setup2 = 'M:\Dev\Repo\VB.net\g4-setup\Setup\bin\Release\\';
-$ftpDirsStrongNetG4Update = 'M:\Dev\Repo\VB.net\g4-update\G4Update\bin\Release\\';
-$ftpDirsStrongNetG4Update2 = 'M:\Dev\Repo\VB.net\g4-update\Update\bin\Release\\';
+$ftpServer = '';
+$ftpUserName = '';
+$ftpPassword = '';
+
+$ftpDirsStrongNetTools = 'E:\Development\Repositories\VB.net\strongnet.tools\StrongNet.Tools\bin\Release\\';
+$ftpDirsStrongNetG4Setup = 'E:\Development\Repositories\VB.net\g4-setup\G4Setup\bin\Release\\';
+$ftpDirsStrongNetG4Setup2 = 'E:\Development\Repositories\VB.net\g4-setup\Setup\bin\Release\\';
+$ftpDirsStrongNetG4Update = 'E:\Development\Repositories\VB.net\g4-update\G4Update\bin\Release\\';
+$ftpDirsStrongNetG4Update2 = 'E:\Development\Repositories\VB.net\g4-update\Update\bin\Release\\';
+$ftpDirsStrongNetG4Update3 = 'E:\Development\Repositories\VB.net\g4-update\G4Upload\bin\Release\\';
 ?>
