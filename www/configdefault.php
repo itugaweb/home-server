d178b2c92684c9e79c0f97cefd42908b127769cc
Horizak <sergioalves87@gmail.com>
2019-01-18 15:21:57 +0000

    modificacao nos paths

--- www/configdefault.php
+++ www/configdefault.php
@@ -1,17 +1,42 @@
 <?php
+function formatPath($params, $fromRoot = true)
+{
+    static $root = null;
+    if ($root === null) {
+        $root = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+    }
+
+    if (!is_array($params))
+        $params = array($params);
+
+    $out = '';
+    if ($fromRoot)
+        $out .= $root;
+
+    foreach ($params as $param)
+        if ($param != '') {
+            $out .= $param . DIRECTORY_SEPARATOR;
+            if (substr($out, -2) == DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR)
+                $out = substr($out, 0, -1);
+        }
+
+    return $out;
+}
+
 $c['company'] = 'CompanyName';
 $c['companyLink'] = 'http://www.company.com';
 $c['debug'] = false;
-$c['dirs']['composer'] = 'libraries/composer/';
-$c['dirs']['images'] = 'images/';
-$c['dirs']['includes'] = 'includes/';
-$c['dirs']['language'] = 'language/';
-$c['dirs']['libraries'] = 'libraries/';
-$c['dirs']['temp'] = 'tmp/';
-$c['dirs']['template'] = 'templates/%s/';
-$c['dirs']['templates'] = 'templates/';
-$c['dirs']['twigTemp'] = sprintf('%stwig/', $c['dirs']['temp']);
-$c['dirs']['twigCache'] = sprintf('%scache/', $c['dirs']['twigTemp']);
+$c['dirs']['root'] = formatPath('');
+$c['dirs']['images'] = formatPath('images');
+$c['dirs']['includes'] = formatPath('includes');
+$c['dirs']['language'] = formatPath('language');
+$c['dirs']['libraries'] = formatPath('libraries');
+$c['dirs']['composer'] = formatPath(array('libraries', 'composer'));
+$c['dirs']['templates'] = formatPath('templates');
+$c['dirs']['template'] = formatPath(array('templates', '%s'));
+$c['dirs']['temp'] = formatPath('tmp');
+$c['dirs']['twigTemp'] = formatPath(array('tmp', 'twig'));
+$c['dirs']['twigCache'] = formatPath(array('tmp', 'twig', 'cache'));
 $c['languagegDefault'] = 'pt-PT';
 $c['template'] = 'templatename';
 $c['year'] = date('Y');
